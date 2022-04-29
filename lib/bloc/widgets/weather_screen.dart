bfc4df1f194cb2b4cd78142917dccc90f53e3071
Ivan Kolchev <ekinsdrow@gmail.com>
2022-04-29 15:46:35 +0300

    Update weather screen ui

--- lib/bloc/widgets/weather_screen.dart
+++ lib/bloc/widgets/weather_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:state_managers_review/bloc/blocs/weather/weather_bloc.dart';
 import 'package:state_managers_review/bloc/data/models/city.dart';
 import 'package:state_managers_review/bloc/di/weather_scope.dart';
 
@@ -19,9 +21,18 @@ class WeatherScreen extends StatelessWidget {
           title: Text('Weather in ${city.name}'),
         ),
         body: Center(
-          child: Text(
-            'Berlin - 45C',
-            style: Theme.of(context).textTheme.headline3,
+          child: BlocBuilder<WeatherBloc, WeatherState>(
+            builder: (context, state) => state.when(
+              loading: () => const CircularProgressIndicator(),
+              success: (weather) => Text(
+                '${city.name} - ${weather.temp}C',
+                style: Theme.of(context).textTheme.headline3,
+              ),
+              error: (e) => Text(
+                e,
+                style: Theme.of(context).textTheme.headline3,
+              ),
+            ),
           ),
         ),
       ),
