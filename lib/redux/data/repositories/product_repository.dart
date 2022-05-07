cccce250602f4662f8bc525360dfa1eeca0dcffe
Ivan Kolchev <ekinsdrow@gmail.com>
2022-05-07 20:37:18 +0300

    Add async to repo

--- lib/redux/data/repositories/product_repository.dart
+++ lib/redux/data/repositories/product_repository.dart
@@ -1,12 +1,12 @@
 import 'package:state_managers_review/redux/data/models/product.dart';
 
 abstract class IProductRepository {
-  List<Product> getProducts();
+  Future<List<Product>> getProducts();
 }
 
 class ProductRepository implements IProductRepository {
   @override
-  List<Product> getProducts() {
+  Future<List<Product>> getProducts() async {
     final products = <Product>[];
 
     for (var i = 0; i < 100; i++) {
@@ -14,6 +14,12 @@ class ProductRepository implements IProductRepository {
         Product(name: 'Product $i'),
       );
     }
+
+    await Future.delayed(
+      const Duration(
+        seconds: 1,
+      ),
+    );
 
     return products;
   }
